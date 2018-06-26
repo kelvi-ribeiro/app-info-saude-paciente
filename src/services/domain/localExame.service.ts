47507b364fceb4ce8eeca7655704e6f3f7f52575
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-06-26 18:41:05 -0300

    Implementado servico que retorna os locais de exame do paciente

---
+++ src/services/domain/localExame.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { API_CONFIG } from "../../config/api.config";
+import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
+
+
+
+@Injectable()
+export class LocalExameService {
+
+  constructor(
+    public http: HttpClient,
+    public storage: StorageService,
+    public imageUtilService: ImageUtilService,
+    public storageService: StorageService,
+    ) {
+  }
+
+  findAllLocaisExameByPacienteId() {
+    let pacienteId = this.storage.getPacienteId()
+    return this.http.get(`${API_CONFIG.baseUrl}/locaisExame?idPaciente=${pacienteId}`);
+  }
+
+  insert(localExame) {
+    return this.http.post(
+      `${API_CONFIG.baseUrl}/locaisExame`,
+      localExame,
+      {
+        observe: 'response',
+        responseType: 'text'
+      }
+    );
+  }
+}
