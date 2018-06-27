f26d2520cae2917e7a016180fac4eb4d4a3fe9b3
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-27 01:15:34 -0300

    Adicionando url do viaCep em api.config

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,7 +1,8 @@
 export const API_CONFIG = {
   //  baseUrl: "http://98b5651f.ngrok.io",
   baseUrl: "http://localhost:8081",
-  bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao"
+  bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-camarao",
+  viaCepUrl:"https://viacep.com.br/ws/"
 
 
 }
