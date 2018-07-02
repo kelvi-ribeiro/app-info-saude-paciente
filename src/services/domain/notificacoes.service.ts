335fd159aa221438d39ae47ed065864a7256e87b
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-02 00:35:51 -0300

    Criando metodos de notificacoes

--- src/services/domain/notificacoes.service.ts
+++ src/services/domain/notificacoes.service.ts
@@ -4,19 +4,51 @@ import { AlertController } from 'ionic-angular/components/alert/alert-controller
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
 import { Observable } from "rxjs/Observable";
+import { ToastController,NavController } from "ionic-angular";
 
 
 
 
 @Injectable()
-export class MedicamentoService {
+export class NotificacoesService {
 
   constructor(
     public http: HttpClient,
     public storage: StorageService,
-    public alertCtrl:AlertController
+    public alertCtrl:AlertController,
+    public toastCtrl:ToastController
+
     ) {
   }
+
+  presentToast(message:string,css:string,duration:number,position:string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: duration,
+      position: position,
+      cssClass:css
+    });
+    toast.present();
+  }
+  presentAlertDefault(title,message,page?,navCtrl?:NavController){
+    let alert = this.alertCtrl.create({
+      title:title,
+      message:message,
+      enableBackdropDismiss:false,
+      buttons:[
+        {
+          text:'Ok',
+          handler:() =>{
+            if(page){
+              navCtrl.setRoot(page)
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 
 }
