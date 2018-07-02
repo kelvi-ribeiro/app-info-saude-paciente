150aec6451f891219dc42e996f3a2180553d2600
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-02 01:47:26 -0300

    Corrigindo problema de pegar mes errado

--- src/services/domain/utils.service.ts
+++ src/services/domain/utils.service.ts
@@ -19,7 +19,7 @@ export class UtilsService {
     // Pega Ano
     // data.substr(data.lastIndexOf('/')+1,data.length -1 )
     let dataFormatada = new Date(parseInt(data.substr(data.lastIndexOf('/')+1,data.length -1 )),
-    parseInt(data.substr(data.indexOf('/')+1,data.lastIndexOf('/')-3)),
+    parseInt(data.substr(data.indexOf('/')+1,data.lastIndexOf('/')-3)) -1,
     parseInt(data.substr(0,data.indexOf('/')))
     );
       return dataFormatada.toISOString().substr(0, 10).split('/').reverse().join('-');
