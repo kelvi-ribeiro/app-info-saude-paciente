2793c31bd07cded2889feb0679dc584b5717fb9c
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-07-02 00:38:03 -0300

    implementando parcialmente update de exame

--- src/pages/form-exame/form-exame.ts
+++ src/pages/form-exame/form-exame.ts
@@ -1,9 +1,10 @@
+import { NotificacoesService } from './../../services/domain/notificacoes.service';
 import { StorageService } from './../../services/storage.service';
-import { LocalExameService } from './../../services/domain/localExame.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { ExameService } from '../../services/domain/exame.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocalExameService } from '../../services/domain/localExame.service';
 
 /**
  * Generated class for the FormExamePage page.
@@ -18,7 +19,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: 'form-exame.html',
 })
 export class FormExamePage {
-
+  exame;
   locaisExame: Object;
   pacienteId: any;
   formGroup:FormGroup;
@@ -27,11 +28,11 @@ export class FormExamePage {
               public navParams: NavParams,
               public storageService:StorageService,
               public formBuilder:FormBuilder,
-              public alertCtrl:AlertController,
+              public notificacoesService:NotificacoesService,
               public exameService:ExameService,
               public localExameService:LocalExameService
               ) {
-
+        this.exame = this.navParams.get('exame');
         this.pacienteId = this.storageService.getPacienteId();
         this.formGroup = this.formBuilder.group({
           nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(60)]],
@@ -52,32 +53,46 @@ export class FormExamePage {
     this.localExameService.findAllLocaisExameByPacienteId()
     .subscribe(res=>{
       this.locaisExame = res;
+      this.verificaUpdate();
     })
   }
   adicionarExame(){
+    if(!this.exame){
+      this.salvarExame()
+    }else{
+      this.atualizarExame()
+    }
+
+  }
+  salvarExame(){
     this.formGroup.controls.data
     .setValue(this.formGroup.value.data.concat(this.formGroup.value.exameDia,' ',this.formGroup.value.exameHora));
     this.formGroup.removeControl('exameDia');
     this.formGroup.removeControl('exameHora');
     this.exameService.insert(this.formGroup.value).subscribe(res=>{
-      this.showInsertOk();
+      this.notificacoesService.presentAlertDefault('Sucesso!','Exame Adicionado','ExamesPage',this.navCtrl)
     })
   }
-  showInsertOk(){
-    let alert = this.alertCtrl.create({
-      title:'Sucesso!',
-      message:'Exame adicionado',
-      enableBackdropDismiss:false,
-      buttons:[
-        {
-          text:'Ok',
-          handler:() =>{
-            this.navCtrl.setRoot('ExamesPage')
-          }
-        }
-      ]
-    });
-    alert.present();
+  atualizarExame(){
+    this.formGroup.controls.data
+    .setValue(this.formGroup.value.data.concat(this.formGroup.value.exameDia,' ',this.formGroup.value.exameHora));
+    this.formGroup.removeControl('exameDia');
+    this.formGroup.removeControl('exameHora');
+    this.exameService.update(this.formGroup.value,this.exame.id).subscribe(res=>{
+      this.notificacoesService.presentAlertDefault('Sucesso!','Exame Atualizado','ExamesPage',this.navCtrl)
+    })
+
+  }
+
+  verificaUpdate(){
+    if(this.exame){
+      this.formGroup.controls.nome.setValue(this.exame.nome);
+      this.formGroup.controls.descricao.setValue(this.exame.descricao);
+      this.formGroup.controls.exameDia.setValue(this.exame.exameDia);
+      this.formGroup.controls.exameHora.setValue(this.exame.exameHora);
+      this.formGroup.controls.localExameId.setValue(this.exame.localExameId);
+
+     }
   }
 
 
