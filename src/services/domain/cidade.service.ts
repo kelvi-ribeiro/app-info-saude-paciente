a269112a30aa6d893a912bcbc9e8f292f3614570
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-15 02:15:08 -0300

    Implementando handler response em cidade.service

--- src/services/domain/cidade.service.ts
+++ src/services/domain/cidade.service.ts
@@ -1,16 +1,33 @@
-
-import { Observable } from 'rxjs/Rx';
-import { HttpClient } from '@angular/common/http';
+import { HandlerResponseProvider } from './../handler-response/handler-response';
 import { Injectable } from "@angular/core";
-import { API_CONFIG } from '../../config/api.config';
-import { CidadeDTO } from '../../models/cidade.dto';
+import { API_CONFIG } from "../../config/api.config";
+import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
+import { Http, Headers } from '@angular/http';
+
 
 @Injectable()
 export class CidadeService{
-  constructor(public http:HttpClient){
+  constructor(
+    public http: Http,
+    public storage: StorageService,
+    public imageUtilService: ImageUtilService,
+    public storageService: StorageService,
+    public handlerResponseService:HandlerResponseProvider
+    ){
 
   }
-  findAll():Observable<CidadeDTO[]>{
-    return this.http.get<CidadeDTO[]>(`${API_CONFIG.baseUrl}/cidades`);
+  findAll(){
+    let headers = new Headers();
+    return this.storage.getUserCredentials()
+    .then(userCredentials=>{
+      headers.append('Authorization', `Bearer ${userCredentials['token']}`)
+      return this.handlerResponseService.handlerResponse(
+        "get",
+        `${API_CONFIG.baseUrl}/cidades`,
+        null,
+        headers
+      );
+    });
   }
 }
