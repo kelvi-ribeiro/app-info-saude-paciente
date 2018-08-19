72b242df205fd6cc918af2fc1cb28211199c536a
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-19 01:48:07 -0300

    Comecando a implementar google maps para os exames

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,10 @@
 export const API_CONFIG = {
   //  baseUrl: "http://98b5651f.ngrok.io",
-  baseUrl: "http://54fc4b6c.ngrok.io",
+  baseUrl: "http://fd52e582.ngrok.io",
   bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/app-comunicacao-fiocruz",
-  viaCepUrl:"https://viacep.com.br/ws/"
-
+  viaCepUrl:"https://viacep.com.br/ws/",
+  urlGeolocation:"https://maps.google.com/maps/api/geocode/json?",
+  apiKeyGeolocation:"AIzaSyAgkZp1A4zstakjAm6rMoWzpvI-2l3anMM"
 
 }
 
