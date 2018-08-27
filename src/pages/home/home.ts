e6b9248a1a8d436cf89580b7f6deaacaf1946f95
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-08-27 00:13:24 -0300

    :sparkles: Terminando logica para calcular a proxima dosagem de medicamento

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -50,54 +50,30 @@ export class HomePage {
 
       })
   }
-  // Método que calcula o horário do medicamento mais próximo
+  // Método que calcula o horário da dosagem mais próximo
   calcularHoraProximoMedicamento(medicamentos) {
-        medicamentos.forEach(medicamento => {
+    medicamentos.forEach(medicamento => {
 
-          const horasRemedio:Date[]  = []
-          const data = new Date()
-          data.setHours(medicamento.horaInicial.substr(0, 2), medicamento.horaInicial.substr(3, 5))
-          const comparadorMedicamentoHoraInicial = new Date()
-          comparadorMedicamentoHoraInicial.setHours(medicamento.horaInicial.substr(0, 2), medicamento.horaInicial.substr(3, 5))
-          do {
-            horasRemedio.push(new Date(data.setTime(data.getTime() + (medicamento.intervaloTempo * 60 * 60 * 1000))))
-          } while ((data.getHours() !== comparadorMedicamentoHoraInicial.getHours()))
-          const dataAtual = new Date()
+      const horasRemedio: Date[] = []
+      const data = new Date()
+      data.setHours(medicamento.horaInicial.substr(0, 2), medicamento.horaInicial.substr(3, 5))
+      const comparadorMedicamentoHoraInicial = new Date()
+      comparadorMedicamentoHoraInicial.setHours(medicamento.horaInicial.substr(0, 2), medicamento.horaInicial.substr(3, 5))
+      do {
+        horasRemedio.push(new Date(data.setTime(data.getTime() + (medicamento.intervaloTempo * 60 * 60 * 1000))))
+      } while ((data.getHours() !== comparadorMedicamentoHoraInicial.getHours()))
+      const dataAtual = new Date()
 
-          let tempData = new Date(horasRemedio[0].getTime())
-          //console.log('HorasRemedio',horasRemedio)
-          for(let i = 0;i <horasRemedio.length;i++){
-            console.log('see',horasRemedio[i].getMilliseconds());
-
-            console.log('horasRemedio',horasRemedio[i].getTime())
-            console.log('dataAtual',dataAtual.getTime())
-              if(dataAtual.getTime() > horasRemedio[i].getTime() && tempData.getTime() <  horasRemedio[i].getTime()){
-                tempData = horasRemedio[i]
-            }
-          }
-          medicamento.proximaHoraMedicamento = tempData
-          console.log('HorasRemedio',horasRemedio)
-        });
-        this.medicamentos = medicamentos
-        // horasRemedio.forEach(horaRemedio => {
-        //   tempHora = horaRemedio;
-        //   console.log('see',horaRemedio.getTime())
-        //   if(horaRemedio.getTime()  <= tempHora.getTime() && dataAtual.getTime() < horaRemedio.getTime()){
-        //     console.log('Chegou aqui',horaRemedio)
-        //   }
-
-        // });
-
-
-      //   horasRemedio.forEach(data => {
-      //     if (tempHora >= data.getHours() - horaAtual.getHours()) {
-      //       medicamento.proximaHoraMedicamento = data
-      //       tempHora = data.getHours() - horaAtual.getHours()
-      //     }
-      //   })
-      // });
-
-
+      for (let i = 0; i < horasRemedio.length; i++) {
+        console.log('horasRemedio', horasRemedio[i].getTime())
+        console.log('dataAtual', dataAtual.getTime())
+        if (horasRemedio[i].getTime() > dataAtual.getTime()) {
+          medicamento.proximaHoraMedicamento = horasRemedio[i + 1]
+          break;
+        }
+      }
+    });
+    this.medicamentos = medicamentos
   }
 }
 
