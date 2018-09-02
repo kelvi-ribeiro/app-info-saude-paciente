2a83b6ac4b1d4bc4896ec408d8c2754bf97eaafc
Kelvi Martins <kelvi.unigranrio@unigranrio.br>
2018-09-02 20:31:10 -0300

    :hammer: Retirando imports e atributos no construtor que nao estao sendo usados

--- src/services/domain/cidade.service.ts
+++ src/services/domain/cidade.service.ts
@@ -2,17 +2,13 @@ import { HandlerResponseProvider } from './../handler-response/handler-response'
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
-import { ImageUtilService } from "../image-util.service";
-import { Http, Headers } from '@angular/http';
+import {Headers } from '@angular/http';
 
 
 @Injectable()
 export class CidadeService{
-  constructor(
-    public http: Http,
-    public storage: StorageService,
-    public imageUtilService: ImageUtilService,
-    public storageService: StorageService,
+  constructor(    
+    public storage: StorageService,       
     public handlerResponseService:HandlerResponseProvider
     ){
 
