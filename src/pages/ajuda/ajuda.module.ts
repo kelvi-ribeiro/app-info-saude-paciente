1d93fb6a4de90eaf94c217d3f9aaa7ba43fc829b
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-10-11 07:16:50 -0300

    Terminando parcialmente css da tela de ajuda

--- src/pages/ajuda/ajuda.module.ts
+++ src/pages/ajuda/ajuda.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AjudaPage } from './ajuda';
+;
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
-    AjudaPage,
+    AjudaPage    
   ],
   imports: [
     IonicPageModule.forChild(AjudaPage),
+    ComponentsModule
   ],
 })
 export class AjudaPageModule {}
--- src/pages/ajuda/ajuda.ts
+++ src/pages/ajuda/ajuda.ts
@@ -15,16 +15,27 @@ import { TextToSpeech, TTSOptions } from '@ionic-native/text-to-speech';
   templateUrl: 'ajuda.html',
 })
 export class AjudaPage {
-  helpSoundTextMedicamento = `Para cadastrar um medicamento, deve-se preencher os campos de nome, descrição, intervalo de tempo para o medicamento, data de início do medicamento, data final para o medicamento, hora inicial e tipo do medicamento, Atenção para os campos de Data inicial, final do medicamento e hora inicial, pois são com estes campos que é calculado a próxima dosagem e dias restantes do medicamento.`
+helpers = [] 
+
+
+  
+  
   constructor(
               public navCtrl: NavController, 
               public navParams: NavParams,
               private tts: TextToSpeech,
               private platform:Platform) {
+        this.helpers.push({title:'Como cadastrar um medicamento ?',
+        text:`Para cadastrar um medicamento, deve-se preencher os campos de nome, descrição, intervalo de tempo para o medicamento, data de início do medicamento, data final para o medicamento, hora inicial e tipo do medicamento. Atenção para os campos de Data inicial, final do medicamento e hora inicial, pois são com estes campos que é calculado a próxima dosagem e dias restantes do medicamento.`,expanded:false,heightText:260})
+
+        this.helpers.push({title:'Como cadastrar um exame ?',
+        text:`Para cadastrar um exame, deve-se preencher os campos de nome, descrição, dia, hora  e local do exame. Deve-se criar um local do exame, para que você possa associar seu exame à algum local`,expanded:false,heightText:150})
+
+        this.helpers.push({title:'Como cadastrar um Local  ?',
+        text:`Para cadastrar local, deve-se preencher os campos de nome, CEP, Número de endereço do local. A partir de um CEP válido, iremos achar o seu endereço.`,expanded:false,heightText:135})
   }
  
-  helpSound(helSoundText){
-    console.log(helSoundText)
+  helpSound(helSoundText){   
     this.platform.ready()
     .then(()=>{
       const options:TTSOptions = {
@@ -34,5 +45,14 @@ export class AjudaPage {
       this.tts.speak(options)
     })  
   }
+  
+
+
+expandItem(item){
+  console.log(this.helpers)
+item.expanded = item.expanded ? false:true
+console.log(this.helpers)
+} 
+  
 
 }
