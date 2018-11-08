684acd70283d860b2cdc24914882ea6f5fa8e073
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-11-08 16:48:43 -0200

    Trocando o texto de algumas mensagens e crando ajuda de mensagens

--- src/pages/ajuda/ajuda.ts
+++ src/pages/ajuda/ajuda.ts
@@ -19,14 +19,17 @@ helpers = []
               public navParams: NavParams,
               private tts: TextToSpeech,
               private platform:Platform) {
-        this.helpers.push({title:'Como cadastrar um medicamento ?',
+        this.helpers.push({title:'Cadastro de medicamento',
         text:`Para cadastrar um medicamento, deve-se preencher os campos de nome, descrição, intervalo de tempo para o medicamento, data de início do medicamento, data final para o medicamento, hora inicial e tipo do medicamento. Atenção para os campos de Data inicial, final do medicamento e hora inicial, pois são com estes campos que é calculado a próxima dosagem e dias restantes do medicamento.`,expanded:false,heightText:260,playing:false})
 
-        this.helpers.push({title:'Como cadastrar um exame ?',
+        this.helpers.push({title:'Cadastro de exame',
         text:`Para cadastrar um exame, deve-se preencher os campos de nome, descrição, dia, hora  e local do exame. Deve-se criar um local do exame, para que você possa associar seu exame à algum local`,expanded:false,heightText:150,playing:false})
 
-        this.helpers.push({title:'Como cadastrar um Local  ?',
-        text:`Para cadastrar local, deve-se preencher os campos de nome, CEP, Número de endereço do local. A partir de um CEP válido, iremos achar o seu endereço.`,expanded:false,heightText:135,playing:false})
+        this.helpers.push({title:'Cadastro de local de exame',
+        text:`Para cadastrar local de exame, deve-se preencher os campos de nome, CEP, Número de endereço do local. A partir de um CEP válido, iremos achar o seu endereço.`,expanded:false,heightText:135,playing:false})
+
+        this.helpers.push({title:'Sobre as mensagens',
+        text:`Mensagens enviados pelos profissionais de saúde, ficam disponíveis na tela de listagem de mensagens, além disso, também é enviado um email contendo as informações da mensagem, isso se mensagem for direcionada para todos os pacientes, alguma linha de cuidado de você participe ou individual`,expanded:false,heightText:200,playing:false})
   }
  
   helpSound(helper){   
