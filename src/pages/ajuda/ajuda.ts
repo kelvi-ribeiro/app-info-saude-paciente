0fc8b8ffd64d23f680bc7bd415fdb1ffe4ff24d4
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-11-08 01:16:16 -0200

    :hammer: adicionando button de parar  a descricao oral e controlando para saber se esta sendo alguma descricao oral ou nao

--- src/pages/ajuda/ajuda.ts
+++ src/pages/ajuda/ajuda.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { TextToSpeech, TTSOptions } from '@ionic-native/text-to-speech';
 
-/**
- * Generated class for the AjudaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -26,24 +20,37 @@ helpers = []
               private tts: TextToSpeech,
               private platform:Platform) {
         this.helpers.push({title:'Como cadastrar um medicamento ?',
-        text:`Para cadastrar um medicamento, deve-se preencher os campos de nome, descrição, intervalo de tempo para o medicamento, data de início do medicamento, data final para o medicamento, hora inicial e tipo do medicamento. Atenção para os campos de Data inicial, final do medicamento e hora inicial, pois são com estes campos que é calculado a próxima dosagem e dias restantes do medicamento.`,expanded:false,heightText:260})
+        text:`Para cadastrar um medicamento, deve-se preencher os campos de nome, descrição, intervalo de tempo para o medicamento, data de início do medicamento, data final para o medicamento, hora inicial e tipo do medicamento. Atenção para os campos de Data inicial, final do medicamento e hora inicial, pois são com estes campos que é calculado a próxima dosagem e dias restantes do medicamento.`,expanded:false,heightText:260,playing:false})
 
         this.helpers.push({title:'Como cadastrar um exame ?',
-        text:`Para cadastrar um exame, deve-se preencher os campos de nome, descrição, dia, hora  e local do exame. Deve-se criar um local do exame, para que você possa associar seu exame à algum local`,expanded:false,heightText:150})
+        text:`Para cadastrar um exame, deve-se preencher os campos de nome, descrição, dia, hora  e local do exame. Deve-se criar um local do exame, para que você possa associar seu exame à algum local`,expanded:false,heightText:150,playing:false})
 
         this.helpers.push({title:'Como cadastrar um Local  ?',
-        text:`Para cadastrar local, deve-se preencher os campos de nome, CEP, Número de endereço do local. A partir de um CEP válido, iremos achar o seu endereço.`,expanded:false,heightText:135})
+        text:`Para cadastrar local, deve-se preencher os campos de nome, CEP, Número de endereço do local. A partir de um CEP válido, iremos achar o seu endereço.`,expanded:false,heightText:135,playing:false})
   }
  
-  helpSound(helSoundText){   
+  helpSound(helper){   
     this.platform.ready()
     .then(()=>{
       const options:TTSOptions = {
-        text:helSoundText,
+        text:helper.text,
         locale:'pt-BR'
       }
+      helper.playing = true;
       this.tts.speak(options)
+      .then(()=>{
+        helper.playing = false;
+      })
     })  
+  }
+  stopSound(helper){    
+    this.platform.ready()
+    .then(()=>{
+      this.tts.speak("")
+      .then(()=>{
+        helper.playing = false
+      })      
+    })    
   }
   
 
