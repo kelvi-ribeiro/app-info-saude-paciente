7a37a732a8a1f5fc47d3db2f379628e2a31032b3
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-11-29 03:34:20 -0200

    implementando funcao de deletar e resturar todos medicamentos e colocando um fab button para acessar essas funcoes

--- src/pages/historico-medicamentos/historico-medicamento.ts
+++ src/pages/historico-medicamentos/historico-medicamento.ts
@@ -1,6 +1,6 @@
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 import { MedicamentoService } from '../../services/domain/medicamento.service';
 import { NotificacoesService } from '../../services/domain/notificacoes.service';
 
@@ -18,7 +18,8 @@ export class HistoricoMedicamentoPage {
               public navParams: NavParams,
               public service:MedicamentoService,
               public alertCtrl:AlertController,
-              public notificacoesService:NotificacoesService) {
+              public notificacoesService:NotificacoesService,
+              public actionSheetCtrl:ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -87,5 +88,103 @@ export class HistoricoMedicamentoPage {
       this.refreshPage()
       this.obterMedicamentosInativos();
     })
-  }  
+  } 
+  presentActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({          
+      buttons: [
+        {
+          text: 'Página Principal',             
+          icon:'home',           
+          handler: () => {
+            this.navCtrl.setRoot('HomePage')
+          }
+        },
+        {
+          text: 'Excluir todos medicamentos',
+          icon:'trash',           
+          handler: () => {
+            this.alertApagarTodosMedicamentos()
+            setTimeout(() => {
+              this.refreshPage()
+            }, 600);
+          }
+        },
+        {
+          text: 'Restaurar todos os Medicamentos',
+          icon:'refresh-circle',           
+          handler: () => {
+            this.alertRestaurarTodosMedicamentos()
+            setTimeout(() => {
+              this.refreshPage()
+            }, 600);
+          
+          }
+        },         
+                      
+        {
+          text: 'Cancelar',            
+          icon:'close',  
+          handler: () => {
+            
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  } 
+  apagarTodosMedicamentos(){    
+     this.medicamentos.forEach(element => {
+      this.service.delete(element.id)
+    }); 
+  }
+  restaurarTodosMedicamentos(){   
+    return this.medicamentos.forEach(element => {
+      this.service.setAtivo(element.id)
+    }); 
+  }
+  alertApagarTodosMedicamentos() {
+    let alert = this.alertCtrl.create({
+      title: "Atenção!",
+      message: 'Deseja mesmo apagar todos os medicamentos ?',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: "Sim",
+          handler: () => {            
+          this.apagarTodosMedicamentos()
+          
+          setTimeout(() => {
+          this.refreshPage()
+            }, 1000);          
+          }
+        },
+        {
+          text: "Não"
+        }
+      ]
+    });
+    alert.present();
+  }
+  alertRestaurarTodosMedicamentos() {
+    let alert = this.alertCtrl.create({
+      title: "Atenção!",
+      message: 'Deseja mesmo restaurar todos os medicamentos ?',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: "Sim",
+          handler: () => {            
+          this.restaurarTodosMedicamentos()
+          setTimeout(() => {
+            this.refreshPage()
+          }, 1000);
+          }
+        },
+        {
+          text: "Não"
+        }
+      ]
+    });
+    alert.present();
+  }
 }
